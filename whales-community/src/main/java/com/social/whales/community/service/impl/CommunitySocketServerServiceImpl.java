802615df2b956c2d3206b6624b51a1211aaf188d
1148973713 <1148973713@qq.com>
2021-08-25 23:31:00 +0800

    SmsMessage

--- whales-community/src/main/java/com/social/whales/community/service/impl/CommunitySocketServerServiceImpl.java
+++ whales-community/src/main/java/com/social/whales/community/service/impl/CommunitySocketServerServiceImpl.java
@@ -1,12 +1,19 @@
 package com.social.whales.community.service.impl;
 
 import com.social.whales.community.service.CommunitySocketServerService;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class CommunitySocketServerServiceImpl implements CommunitySocketServerService {
+    @Resource
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @Override
     public void sendGroupAndDataProcess(String groupId) {
-
+        //TODO 放入Redis缓存再通过rabbit从存入数据库
+        simpMessagingTemplate.convertAndSend("/"+groupId);
     }
 }
